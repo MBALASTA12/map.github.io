ffc34ba7d6816a3f96322c25e2b21bdf0ab46b66
MBALASTA12 <nk.marlonbalasta@gmail.com>
2024-09-29 14:20:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
+// script.js
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Welcome to My Web App!");
-
-    // Initialize the map and set the initial view to General Santos City
-    var map = L.map('map').setView([6.1026, 125.1716], 13);
-
-    // Add OpenStreetMap tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // Update the map view on movement (no marker)
-    map.on('move', function() {
-        // No marker to update; this can be used for other functionalities if needed
-    });
 });
