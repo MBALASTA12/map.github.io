658fb10a502ec244b69dee0245d928c09a1969d3
MBALASTA12 <nk.marlonbalasta@gmail.com>
2024-10-01 19:40:02 +0800

    Create drivermap.js

---
+++ js/drivermap.js
@@ -0,0 +1,9 @@
+// Initialize map centered on General Santos City
+var map = L.map('map', {
+    zoomControl: false, // Remove zoom buttons
+}).setView([6.1164, 125.1716], 13); // Coordinates of General Santos City
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
